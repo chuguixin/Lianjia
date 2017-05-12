a7906109488c83466a9a4329a1fa53348d4aaccd
chuguixin <chuguixin@163.com>
2017-05-12 09:43:55 +0800

    modify deal

--- scrapy/lianjia/spiders/deal.py
+++ scrapy/lianjia/spiders/deal.py
@@ -1,26 +1,26 @@
 # -*- coding: utf-8 -*-
-import sys
-reload(sys)
-sys.setdefaultencoding("utf-8")
+import sys  
+reload(sys)  
+sys.setdefaultencoding('utf8')
 import scrapy
-from lxml import etree
 import json
 import re
 import time
 import math
-from ..items import LianjiaItem
+import os
+import datetime
 
 
 
 class DealSpider(scrapy.Spider):
     name = "deal"
-    cityDomain = "jn"
+    cityDomain = "qd"
     allowed_domains = [cityDomain + ".lianjia.com"]
-    user_agent = 'Mozil data-role="ershoufang"la/5.0 (Macintosh; Intel Mac OS X 10_12_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36'
+    user_agent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36'
     headers = {'User-Agent': user_agent}
     start_url = 'http://' + cityDomain + '.lianjia.com/chengjiao/'
-    allHouseData = {}
-    houseDict = {}
+    areaDealData = {}
+    allDealData = {}
 
     def start_requests(self):
         yield scrapy.Request(url=self.start_url, headers=self.headers, method='GET', callback=self.dataInit)
@@ -31,7 +31,7 @@ class DealSpider(scrapy.Spider):
                 areaHanzi = area.xpath('text()').extract()[0]
                 areaPinyin = area.xpath('@href').extract()[0].split('/')[2]
                 areaName = areaHanzi + areaPinyin
-                self.allHouseData[areaName + "Dict"] = {}
+                self.areaDealData[areaName] = {}
                 areaUrl = ('http://' + self.cityDomain + '.lianjia.com/chengjiao/{}/').format(areaPinyin)
                 yield scrapy.Request(url=areaUrl, headers=self.headers, callback=self.areaRequest, meta={"areaHanzi":areaHanzi,"areaPinyin":areaPinyin} )
             except Exception:
@@ -41,22 +41,46 @@ class DealSpider(scrapy.Spider):
         areaPinyin = response.meta["areaPinyin"]
         areaName = areaHanzi + areaPinyin
         houseId = int(response.meta["houseId"])
+        dealDataStr = response.css('.house-title .wrapper span::text').extract()[0][0:10]
+        dealDate = time.strptime(dealDataStr, "%Y.%m.%d")
+        dealDatetime = datetime.datetime.strptime(dealDataStr, "%Y.%m.%d")
+        dealYear,dealMonth,dealDay = dealDate[0:3]
+        dealWeekDay, = dealDate[6:7]
+        if dealWeekDay > 0:
+            dealDataStr = (dealDatetime - (datetime.timedelta(days=dealWeekDay))).strftime("%Y.%m.%d")
+        if not self.allDealData.has_key(dealDataStr):
+            self.allDealData[dealDataStr] = []
         houseItemDict = {}
+        #房子url
         houseItemDict["url"] = str(response.url)
+        #房子名称
         houseItemDict["title"] = str(response.css('.house-title .wrapper::text').extract()[0])
+        #成交价格
         houseItemDict["price"] = float(response.css('.overview .price .dealTotalPrice i::text').extract()[0])
-        houseItemDict["unitPrice"] = int(response.css('.overview .price b::text').extract()[0])
-        houseItemDict["lastOnlinePrice"] = float(response.css('.overview .info .msg span')[0].css('label::text').extract()[0])
-        houseItemDict["onlineTime"] = int(response.css('.overview .info .msg span')[1].css('label::text').extract()[0])
-        houseItemDict["modifyPriceTime"] = int(response.css('.overview .info .msg span')[2].css('label::text').extract()[0])
-        houseItemDict["visitTime"] = int(response.css('.overview .info .msg span')[3].css('label::text').extract()[0])
-        houseItemDict["focusPeopleCount"] = int(response.css('.overview .info .msg span')[4].css('label::text').extract()[0])
-        houseItemDict["houseLayout"] = str(response.css('#introduction .introContent .content ul li')[0].css("::text").extract()[1])
-        houseItemDict["houseYears"] = str(response.css('#introduction .introContent .content ul li')[12].css("::text").extract()[1])
-        houseItemDict["startOnlineTime"] = str(response.css('#introduction .transaction .content ul li')[0].css("::text").extract()[1])
-        houseItemDict["housePurpose"] = str(response.css('#introduction .transaction .content ul li')[0].css("::text").extract()[1])
-        self.houseDict[houseId] = houseItemDict
-        self.allHouseData[areaName + "Dict"][houseId] = houseItemDict
+        #成交单价
+        houseItemDict["unitPrice"] = int(response.css('.overview .price b::text').extract()[0]) 
+        #最后挂牌价
+        try:
+            houseItemDict["lastOnlinePrice"] = float(response.css('.overview .info .msg span')[0].css('label::text').extract()[0])
+        except Exception:
+            houseItemDict["lastOnlinePrice"] = 0
+        #成交周期
+        try:
+            houseItemDict["dealUsedDays"] = int(response.css('.overview .info .msg span')[1].css('label::text').extract()[0])
+        except Exception:
+            houseItemDict["dealUsedDays"] = 0
+        #成交带看次数
+        houseItemDict["dealUsedVisitTimes"] = int(response.css('.overview .info .msg span')[3].css('label::text').extract()[0])
+        #建筑面积
+        houseAreaCountStr = str(response.css('#introduction .introContent .base .content ul li')[2].css("::text").extract()[1])
+        reMatchObj = re.search( r'^([\d|\.]+).*', houseAreaCountStr, re.M|re.I)
+        houseAreaCount = float(reMatchObj.group(1))
+        houseItemDict["houseAreaCount"] = houseAreaCount
+
+        self.allDealData[dealDataStr].append(houseItemDict)
+        if not self.areaDealData[areaName].has_key(dealDataStr):
+            self.areaDealData[areaName][dealDataStr] = []
+        self.areaDealData[areaName][dealDataStr].append(houseItemDict)
     def areaParse(self,response):
         areaHanzi = response.meta["areaHanzi"]
         areaPinyin = response.meta["areaPinyin"]
@@ -66,6 +90,11 @@ class DealSpider(scrapy.Spider):
             houseUrl = house.css('.title a::attr(href)').extract()[0]
             reMatchObj = re.search( r'[\D]*(\d+).*', houseUrl, re.M|re.I)
             houseId = int(reMatchObj.group(1))
+            dealDataStr = house.css('.info .dealDate::text').extract()[0]
+            dealDate = time.strptime(dealDataStr, "%Y.%m.%d")
+            dealYear,dealMonth,dealDay = dealDate[0:3]
+            if (dealYear < 2013):
+                return
             yield scrapy.Request(url=houseUrl, 
                                 headers=self.headers, 
                                 callback=self.detailParse, 
@@ -85,6 +114,17 @@ class DealSpider(scrapy.Spider):
             url = ('http://' + self.cityDomain + '.lianjia.com/chengjiao/{}/pg{}/').format(response.meta["areaPinyin"],str(i))
             yield scrapy.Request(url=url, headers=self.headers, callback=self.areaParse, meta={"areaHanzi":areaHanzi,"areaPinyin":areaPinyin} )
     def closed(self, reason):
-        self.allHouseData['houseDict'] = self.houseDict
-        with open(time.strftime("%Y-%m-%d", time.localtime()) + '.' + self.cityDomain + '.deal.json', 'wb') as json_file:
-            json_file.write(json.dumps(self.allHouseData, indent=4, sort_keys=False, ensure_ascii=False))
+        allData = {}
+        allData['areaDealData'] = self.areaDealData
+        allData['allDealData'] = self.allDealData
+        path = os.path.dirname(os.path.abspath(__file__))
+        if os.path.isdir(path):
+            jsonFileWiriteDir = path
+        elif os.path.isfile(path):
+            jsonFileWiriteDir = os.path.dirname(path)
+        jsonFileWiriteDir = jsonFileWiriteDir + '/../../../scrapy-data/raw/'
+        if (not os.path.isdir(jsonFileWiriteDir)):
+            os.mkdir(jsonFileWiriteDir)
+        os.chdir(jsonFileWiriteDir)
+        with open(jsonFileWiriteDir + time.strftime("%Y-%m-%d", time.localtime()) + '.' + self.cityDomain + '.deal.json', 'wb') as json_file:
+            json_file.write(json.dumps(allData, indent=4, sort_keys=False, ensure_ascii=False))
