b9af95a5a3e96c9e5648bce64db4444335097f42
chuguixin <chuguixin@163.com>
2017-05-21 08:25:49 +0800

    request list

--- scrapy/lianjia/spiders/deal.py
+++ scrapy/lianjia/spiders/deal.py
@@ -14,24 +14,26 @@ import datetime
 
 class DealSpider(scrapy.Spider):
     name = "deal"
-    cityDomain = "qd"
+    cityDomain = "sz"
     allowed_domains = [cityDomain + ".lianjia.com"]
     user_agent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36'
     headers = {'User-Agent': user_agent}
     start_url = 'http://' + cityDomain + '.lianjia.com/chengjiao/'
+    cookies = {
+    }
     areaDealData = {}
     allDealData = {}
 
     def start_requests(self):
-        yield scrapy.Request(url=self.start_url, headers=self.headers, method='GET', callback=self.dataInit)
+        yield scrapy.Request(url=self.start_url, headers=self.headers, cookies=self.cookies, method='GET', callback=self.dataInit)
     def dataInit(self, response):
         areaList = response.css('.m-filter .position div[data-role=ershoufang] a')
         for area in areaList:
-            time.sleep(3)
             try:
+                # time.sleep(3)
                 areaHanzi = area.xpath('text()').extract()[0]
                 areaPinyin = area.xpath('@href').extract()[0].split('/')[2]
-                areaName = areaHanzi + areaPinyin
+                areaName = areaPinyin
                 self.areaDealData[areaName] = {}
                 areaUrl = ('http://' + self.cityDomain + '.lianjia.com/chengjiao/{}/').format(areaPinyin)
                 yield scrapy.Request(url=areaUrl, headers=self.headers, callback=self.areaRequest, meta={"areaHanzi":areaHanzi,"areaPinyin":areaPinyin} )
@@ -40,7 +42,9 @@ class DealSpider(scrapy.Spider):
     def detailParse(self, response):
         areaHanzi = response.meta["areaHanzi"]
         areaPinyin = response.meta["areaPinyin"]
-        areaName = areaHanzi + areaPinyin
+        subAreaPinyin = response.meta["subAreaPinyin"]
+        subAreaHanzi = response.meta["subAreaHanzi"]
+        areaName = areaPinyin
         houseId = int(response.meta["houseId"])
         dealDataStr = response.css('.house-title .wrapper span::text').extract()[0][0:10]
         dealDate = time.strptime(dealDataStr, "%Y.%m.%d")
@@ -82,10 +86,12 @@ class DealSpider(scrapy.Spider):
         if not self.areaDealData[areaName].has_key(dealDataStr):
             self.areaDealData[areaName][dealDataStr] = []
         self.areaDealData[areaName][dealDataStr].append(houseItemDict)
-    def areaParse(self,response):
+    def subAreaParse(self,response):
         areaHanzi = response.meta["areaHanzi"]
         areaPinyin = response.meta["areaPinyin"]
-        areaName = areaHanzi + areaPinyin
+        subAreaPinyin = response.meta["subAreaPinyin"]
+        subAreaHanzi = response.meta["subAreaHanzi"]
+        areaName = areaPinyin
         housePriceArray = response.css('ul.listContent li')
         for house in housePriceArray:
             houseUrl = house.css('.title a::attr(href)').extract()[0]
@@ -94,28 +100,68 @@ class DealSpider(scrapy.Spider):
             dealDataStr = house.css('.info .dealDate::text').extract()[0]
             dealDate = time.strptime(dealDataStr, "%Y.%m.%d")
             dealYear,dealMonth,dealDay = dealDate[0:3]
-            if (dealYear < 2013):
-                return
-            time.sleep(1)
-            yield scrapy.Request(url=houseUrl, 
-                                headers=self.headers, 
-                                callback=self.detailParse, 
-                                meta={"areaHanzi":areaHanzi,"areaPinyin":areaPinyin, "houseId":houseId} )
-            
-    def areaRequest(self,response):
-        areaHanzi = response.meta["areaHanzi"]
+            houseItemDict = {}
+            houseItemDict["url"] = houseUrl
+            houseItemDict["price"] = float(response.css('.info .address .totalPrice span::text').extract()[0])
+            houseItemDict["unitPrice"] = int(response.css('.info .flood .unitPrice .number::text').extract()[0]) 
+            houseItemDict["areaHanzi"] = areaHanzi
+            houseItemDict["subAreaHanzi"] = subAreaHanzi
+            if not self.allDealData.has_key(dealDataStr):
+                self.allDealData[dealDataStr] = []
+            self.allDealData[dealDataStr].append(houseItemDict)
+            if not self.areaDealData[areaName].has_key(dealDataStr):
+                self.areaDealData[areaName][dealDataStr] = []
+            self.areaDealData[areaName][dealDataStr].append(houseItemDict)
+    
+    def subAreaRequest(self,response):
         areaPinyin = response.meta["areaPinyin"]
-        areaName = areaHanzi + areaPinyin
+        areaHanzi = response.meta["areaHanzi"]
+        subAreaPinyin = response.meta["subAreaPinyin"]
+        subAreaHanzi = response.meta["subAreaHanzi"]
         areaTotalCount = int(response.css('.leftContent .resultDes .total span::text').extract()[0])
         pageCount = 0;
         if (areaTotalCount <= 0): 
             return
         else:
-            pageCount = int(math.ceil(areaTotalCount/30) * 2)
+            pageCount = int(math.ceil(areaTotalCount/30) * 1.1) + 1
+        if (pageCount > 101):
+            pageCount = 101
         for i in range(1,pageCount):
-            time.sleep(3)
-            url = ('http://' + self.cityDomain + '.lianjia.com/chengjiao/{}/pg{}/').format(response.meta["areaPinyin"],str(i))
-            yield scrapy.Request(url=url, headers=self.headers, callback=self.areaParse, meta={"areaHanzi":areaHanzi,"areaPinyin":areaPinyin} )
+            url = ('http://' + self.cityDomain + '.lianjia.com/chengjiao/{}/pg{}/').format(subAreaPinyin,str(i))
+            yield scrapy.Request(
+                url=url, 
+                headers=self.headers, 
+                callback=self.subAreaParse, 
+                meta={
+                    "areaHanzi":areaHanzi,
+                    "areaPinyin":areaPinyin,
+                    "subAreaHanzi":subAreaHanzi,
+                    "subAreaPinyin":subAreaPinyin
+                })
+
+    def areaRequest(self,response):
+        areaHanzi = response.meta["areaHanzi"]
+        areaPinyin = response.meta["areaPinyin"]
+        areaName = areaPinyin
+        subAreaList = response.css('.m-filter .position div[data-role=ershoufang] div')[1].css('a')
+        for subArea in subAreaList:
+            try:
+                subAreaHanzi = subArea.xpath('text()').extract()[0]
+                subAreaPinyin = subArea.xpath('@href').extract()[0].split('/')[2]
+                self.areaDealData[areaName] = {}
+                subAreaUrl = ('http://' + self.cityDomain + '.lianjia.com/chengjiao/{}/').format(subAreaPinyin)
+                yield scrapy.Request(
+                    url=subAreaUrl, 
+                    headers=self.headers, 
+                    callback=self.subAreaRequest, 
+                    meta={
+                        "areaHanzi":areaHanzi,
+                        "areaPinyin":areaPinyin,
+                        "subAreaHanzi":subAreaHanzi,
+                        "subAreaPinyin":subAreaPinyin
+                    })
+            except Exception:
+                pass
     def closed(self, reason):
         allData = {}
         allData['areaDealData'] = self.areaDealData
