9daae66b7ae608bc276c53e5713c5edd8b376df1
chuguixin <chuguixin@163.com>
2017-05-23 14:08:10 +0800

    add beijing

--- scrapy-data/handle-deal.py
+++ scrapy-data/handle-deal.py
@@ -10,7 +10,7 @@ import time
 import math
 import os
 
-with open('./raw/2017-05-21.sz.deal.json') as json_file:
+with open('./raw/2017-05-23.bj.deal.json') as json_file:
     houseDealData = json.load(json_file)
 
 exportResultJson = {}
@@ -18,7 +18,9 @@ areaNameList = []
 echarsMonthDict = {}
 echartsJson = {
     'unitPrice': [],
-    'dealCount': []
+    'dealCount': [],
+    'unitAreaPrice': [],
+    'areaCount': []
 }
 
 areaDealDataItems = houseDealData['areaDealData'].items()
@@ -29,6 +31,8 @@ for areaKey,areaValue in areaDealDataItems:
     areaMonthData = {}
     echartsSeriesDataUnitPrice = []
     echartsSeriesDataHouseCount = []
+    echartsSeriesDataAreaUnitPrice = []
+    echartsSeriesDataAreaCount = []
     if (len(areaDateDealItems) < 1):
         continue
     # 单个区循环，每次为每天
@@ -41,7 +45,8 @@ for areaKey,areaValue in areaDealDataItems:
             areaMonthData[month] = {
                 'priceCount': 0.0,
                 'houseCount': 0,
-                'unitPriceCount': 0.0
+                'unitPriceCount': 0.0,
+                'areaCount': 0.0
             }
             echarsMonthDict[month] = 1
         # 循环每天所有的成交，并记录到当天所在的月
@@ -49,6 +54,7 @@ for areaKey,areaValue in areaDealDataItems:
             areaMonthData[month]['priceCount'] = areaMonthData[month]['priceCount'] + dealItem['price'];
             areaMonthData[month]['houseCount'] = areaMonthData[month]['houseCount'] + 1;
             areaMonthData[month]['unitPriceCount'] = areaMonthData[month]['unitPriceCount'] + dealItem['unitPrice'];
+            areaMonthData[month]['areaCount'] = areaMonthData[month]['areaCount'] + dealItem['houseAreaCount'];
             if (areaName != dealItem['areaHanzi']):
                 areaName = areaName + dealItem['areaHanzi']
                 areaNameList.append(dealItem['areaHanzi'])
@@ -56,21 +62,41 @@ for areaKey,areaValue in areaDealDataItems:
     areaMonthDataItems = areaMonthData.items()
     for monthKey, monthValue in areaMonthDataItems:
         calcUnitPrice = round(monthValue['unitPriceCount']/(monthValue['houseCount']*10000), 2)
+        calcAreaUnitPrice = round(monthValue['priceCount']/monthValue['areaCount'], 2)
         monthValue['calcUnitPrice'] = calcUnitPrice
+        monthValue['calcAreaUnitPrice'] = calcAreaUnitPrice
         # 为echarts data添加数据
         echartsSeriesDataUnitPrice.append([monthKey, calcUnitPrice])
         echartsSeriesDataHouseCount.append([monthKey, monthValue['houseCount']])
+        echartsSeriesDataAreaCount.append([monthKey, monthValue['areaCount']])
+        echartsSeriesDataAreaUnitPrice.append([monthKey, calcAreaUnitPrice])
     # 
+    echartsJson['unitAreaPrice'].append({
+        'name': areaName,
+        'data': echartsSeriesDataAreaUnitPrice,
+        'type': 'line',
+        'smooth': True,
+        'stack': areaKey
+    })
     echartsJson['unitPrice'].append({
         'name': areaName,
         'data': echartsSeriesDataUnitPrice,
         'type': 'line',
+        'smooth': True,
         'stack': areaKey
     })
     echartsJson['dealCount'].append({
         'name': areaName,
         'data': echartsSeriesDataHouseCount,
         'type': 'line',
+        'smooth': True,
+        'stack': areaKey
+    })
+    echartsJson['areaCount'].append({
+        'name': areaName,
+        'data': echartsSeriesDataAreaCount,
+        'type': 'line',
+        'smooth': True,
         'stack': areaKey
     })
 
@@ -97,7 +123,7 @@ jsonFileWiriteDir = jsonFileWiriteDir + '/dist/'
 if (not os.path.isdir(jsonFileWiriteDir)):
     os.mkdir(jsonFileWiriteDir)
 os.chdir(jsonFileWiriteDir)
-with open(jsonFileWiriteDir + time.strftime("%Y-%m-%d", time.localtime()) + '.' + 'sz.json', 'wb') as json_file:
+with open(jsonFileWiriteDir + time.strftime("%Y-%m-%d", time.localtime()) + '.' + 'bj.json', 'wb') as json_file:
     json_file.write(json.dumps(exportResultJson, indent=4, sort_keys=False, ensure_ascii=False))
-with open(jsonFileWiriteDir + time.strftime("%Y-%m-%d", time.localtime()) + '.' + 'sz.echarst.json', 'wb') as json_file:
+with open(jsonFileWiriteDir + time.strftime("%Y-%m-%d", time.localtime()) + '.' + 'bj.echarst.json', 'wb') as json_file:
     json_file.write(json.dumps(echartsJson, indent=4, sort_keys=False, ensure_ascii=False))
